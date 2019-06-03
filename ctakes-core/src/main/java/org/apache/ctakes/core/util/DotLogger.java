f93ed1b7e643986a6ce6a3edb7a2e05f0dad5dd2
Sean Finan <seanfinan@apache.org>
2019-06-03 11:57:24 +0000

    Use executor, dot every 0.5 seconds
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1860552 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-core/src/main/java/org/apache/ctakes/core/util/DotLogger.java
+++ ctakes-core/src/main/java/org/apache/ctakes/core/util/DotLogger.java
@@ -2,11 +2,14 @@ package org.apache.ctakes.core.util;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Dot Logger Usable in try as resource blocks
+ * Dot Logger Usable in try as resource blocks.  Logs a dot every 0.5 seconds until a process completes.
  *
  * @author SPF , chip-nlp
  * @version %I%
@@ -17,14 +20,15 @@ final public class DotLogger implements Closeable {
    static private final org.apache.log4j.Logger DOT_LOGGER = org.apache.log4j.Logger.getLogger( "ProgressAppender" );
    static private final org.apache.log4j.Logger EOL_LOGGER = org.apache.log4j.Logger.getLogger( "ProgressDone" );
 
-   private final Timer _timer;
+   private final ExecutorService _timer;
 
    /**
     * Starts the Dot Logging
     */
    public DotLogger() {
-      _timer = new Timer();
-      _timer.scheduleAtFixedRate( new DotPlotter(), 333, 333 );
+      _timer = Executors.newScheduledThreadPool( 1 );
+      ((ScheduledExecutorService)_timer).scheduleAtFixedRate(
+            new DotPlotter(), 500, 500, TimeUnit.MILLISECONDS );
    }
 
    /**
@@ -32,19 +36,23 @@ final public class DotLogger implements Closeable {
     */
    @Override
    public void close() throws IOException {
-      _timer.cancel();
-      EOL_LOGGER.error( "" );
+      _timer.shutdownNow();
+      EOL_LOGGER.info( "" );
    }
 
-   static private class DotPlotter extends TimerTask {
+   private class DotPlotter extends TimerTask {
       private int _count = 0;
 
       @Override
       public void run() {
          DOT_LOGGER.info( "." );
          _count++;
-         if ( _count % 30 == 0 ) {
-            EOL_LOGGER.info( " " + (_count / 3) );
+         if ( _count % 60 == 0 ) {
+            if ( _count % 120 == 0 ) {
+               EOL_LOGGER.info( " " + (_count / 2) );
+            } else {
+               DOT_LOGGER.info( " " + (_count / 2) + " " );
+            }
          }
       }
    }
